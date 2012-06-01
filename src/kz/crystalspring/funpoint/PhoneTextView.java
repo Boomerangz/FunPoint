39b4c18fdbed8dd9fe367506bb7c69a89bd21a20
Igor Zygin <izygin@gmail.com>
2012-06-01 15:38:37 +0600

    Телефоны

---
+++ src/kz/crystalspring/funpoint/PhoneTextView.java
@@ -0,0 +1,46 @@
+package kz.crystalspring.funpoint;
+
+import kz.crystalspring.pointplus.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class PhoneTextView extends LinearLayout
+{
+	TextView text;
+	public PhoneTextView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		init();
+	}
+	
+	public PhoneTextView(Context context)
+	{
+		super(context);
+		init();
+	}
+	
+	public void init()
+	{
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li = (LayoutInflater) getContext().getSystemService(
+				infService);
+		li.inflate(R.layout.phone_textview, this, true);
+
+		text = (TextView) findViewById(R.id.phone);
+	}
+	
+	public void setPhone(String phone)
+	{
+		text.setText(phone);
+	}
+	
+	public String getPhone()
+	{
+		return text.getText().toString();
+	}
+	
+
+}
