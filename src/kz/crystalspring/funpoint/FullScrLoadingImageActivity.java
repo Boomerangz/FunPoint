a59c10c320c658c8050878e9d716a2f43b60a5fa
Igor Zygin <izygin@gmail.com>
2012-06-13 15:28:17 +0600

    Изображние с зумом
    Плохо задан начальный зум

--- src/kz/crystalspring/funpoint/FullScrLoadingImageActivity.java
+++ src/kz/crystalspring/funpoint/FullScrLoadingImageActivity.java
@@ -3,22 +3,66 @@ package kz.crystalspring.funpoint;
 import kz.crystalspring.funpoint.venues.FSQConnector;
 import kz.crystalspring.funpoint.venues.OptionalInfo.UrlDrawable;
 import kz.crystalspring.visualities.LoadingImageView;
+import kz.crystalspring.visualities.TouchImageView;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.ProgressBar;
 
 public class FullScrLoadingImageActivity extends Activity
 {
-	LoadingImageView iv;
+	TouchImageView iv;
+	ProgressBar progressbar;
+	UrlDrawable urlDr;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.full_scr_loading_image);
-		iv=(LoadingImageView) findViewById(R.id.loading_image);
-		UrlDrawable urlDr=MainApplication.selectedItemPhoto;
-		if (urlDr!=null)
+		iv = (TouchImageView) findViewById(R.id.photo);
+		progressbar = (ProgressBar) findViewById(R.id.progress_bar);
+		urlDr = MainApplication.selectedItemPhoto;
+		if (urlDr != null)
 		{
-			FSQConnector.loadImageAsync(iv, urlDr, UrlDrawable.BIG_URL,true);
+			if (urlDr.getBigDrawable() == null)
+			// FSQConnector.loadImageAsync(iv, urlDr, UrlDrawable.BIG_URL,true);
+
+			{
+				Runnable task = new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						Drawable dr = FSQConnector
+								.loadPictureByUrl(urlDr.bigUrl);
+						urlDr.setBigDrawable(dr);
+					}
+				};
+
+				Runnable postTask = new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						tuneImage();
+					}
+				};
+
+				MainApplication.pwAggregator.addPriorityTask(task, postTask);
+			} else
+				tuneImage();
 		}
+	}
+
+	private void tuneImage()
+	{
+		progressbar.setVisibility(View.GONE);
+		Drawable dr=urlDr.getBigDrawable();
+		iv.setImageDrawable(dr);
+		iv.setVisibility(View.VISIBLE);
 	}
 }
--- src/kz/crystalspring/funpoint/venues/OptionalInfo.java
+++ src/kz/crystalspring/funpoint/venues/OptionalInfo.java
@@ -30,8 +30,8 @@ public class OptionalInfo
 		public static final int BIG_URL=1;
 		public static final int SMALL_URL=2;
 		
-		String bigUrl="";
-		String smallUrl="";
+		public String bigUrl="";
+		public String smallUrl="";
 		private Drawable bigDrawable=null;
 		private Drawable smallDrawable=null;
 		public synchronized Drawable getBigDrawable()
